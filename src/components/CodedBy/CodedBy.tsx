035926a8a877ab8c177997e45ab7a17ccf299986
stron <yaneliyahu83@gmail.com>
2023-11-11 14:26:19 +0200

    Add CodedBy.tsx component

---
+++ src/components/CodedBy/CodedBy.tsx
@@ -0,0 +1,13 @@
+import "./CodedBy.scss";
+
+export default function CodedBy() {
+  return (
+    <p className="coded-by">
+      Challenge by&nbsp;
+      <a href="https://www.frontendmentor.io/challenges/rest-countries-api-with-color-theme-switcher-5cacc469fec04111f7b848ca">
+        Frontend Mentor
+      </a>
+      . Coded by <a href="https://github.com/itsyanQA">Yan Eliyahu</a>.
+    </p>
+  );
+}
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import Content from "../../components/Content/Content";
+import CodedBy from "../../components/CodedBy/CodedBy";
 
 export default function HomePage() {
   return (
     <>
       <Header />
       <Content />
+      <CodedBy />
     </>
   );
 }
